14990ad584c42f3ec19e4aca9904a38f8af7ca0b
Aarif Alam <aariffulalam36@gmail.com>
2024-09-13 12:35:59 +0530

    up

---
+++ app/routes/action.tsx
@@ -0,0 +1,11 @@
+export async function loader() {
+    // provides data to the component
+  }
+  
+  export default function Component() {
+    // renders the UI
+  }
+  
+  export async function action() {
+    // updates persistent data
+  }
