e0d9089700b539bb5272629e40906358609d3da3
Aarif Alam <aariffulalam36@gmail.com>
2024-09-26 02:58:18 +0530

    signup page created

--- app/routes/signup/index.tsx
+++ app/routes/signup/index.tsx
@@ -17,8 +17,18 @@ export default function signup(){
                 Welcome to Signup Page.
             </h1>
             <Form method="post">
-                <input type="name" name="name"/><br></br>
-                <button type="submit">Sumbmit</button>
+                <label htmlFor="first_name" style={{backgroundColor:'white', border:'inset', margin:'2px'}}>First Name - </label>
+                <input type="text" name="first_name" style={{border:'outset', margin:'2px'}}/><br></br>
+
+                <label htmlFor="last_name" style={{backgroundColor:'white', border:'inset', margin:'2px'}}>Last Name - </label>
+                <input type="text" name="last_name" style={{border:'outset', margin:'2px'}} /><br></br>
+
+                <label htmlFor="email" style={{backgroundColor:'white', border:'inset', margin:'2px'}}>Email - </label>
+                <input type="email" name="email" style={{border:'outset', margin:'2px'}} /><br></br>
+                
+                <label htmlFor="mobile" style={{backgroundColor:'white', border:'inset', margin:'2px'}}>Mobile - </label>
+                <input type="mobile" name="mobile" style={{border:'outset', margin:'2px'}} /><br></br>
+                <button type="submit" style={{border:'solid', borderRadius: '10px', margin:'2px', backgroundColor:'white'}}>Sumbmit</button>
             </Form>
             {actionData != undefined && actionData ? <h6>Name submit successfully</h6>: actionData != undefined && !actionData ?<h6>Name is not submited</h6>: <></>}
         </div>
